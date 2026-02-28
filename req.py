24dcdfadcae5256938848e998257bff19a1fd44f
Vladimir <Vladimir@mail.ru>
2026-02-28 08:51:41 +0500

    delete ips.txt

--- req.py
+++ req.py
@@ -3,6 +3,5 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKENSENDIP")
 
-print(BOT_TOKEN)
-with open("ips.txt", "r", encoding="utf-8") as ips:
+with open("/root/bots/testBots/sendIps/ips.txt", "r", encoding="utf-8") as ips:
     print(requests.get(f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id=1648413619&text={ips.read()}").json())
