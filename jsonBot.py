7890f74915a243facce2ada808b406932fd538b7
Vladimir <Vladimir@mail.ru>
2026-03-09 10:25:40 +0500

    add a new dir, add jsonBot

---
+++ jsonBot.py
@@ -0,0 +1,32 @@
+import json
+import re
+import aiogram
+from aiogram.filters import Command
+from aiogram.types import Message
+import subprocess
+
+users = []
+
+pattern = re.compile(r"(?:user|inbound)>>>(.*?)>>>traffic>>>(.*)")
+
+result = subprocess.run(
+    ["xray", "api", "statsquery", "--server=127.0.0.1:10085"],
+    capture_output=True,
+    text=True
+)
+
+
+with open("/root/bots/testBots/jsonOutput.txt", "w", encoding="utf-8") as out:
+    data = json.loads(result.stdout)
+    stat = data['stat']
+    for i in stat:
+        value = i['value']
+        name = i['name']
+        m = pattern.search(name)
+        if m:
+            mbit = value * 8 / 1_000_000
+            users.append(f'{m.group(1)} {m.group(2)} {mbit:.2f} Mbit')
+    out.write("\n".join(users))
+
+
+
