eef57ebd226e39f4d62818223239ceb4eb831103
Vladimir <Vladimir@mail.ru>
2026-03-14 08:50:29 +0500

    add correctly output and fix FileNotFoundError

--- sendIps/readText.py
+++ sendIps/readText.py
@@ -10,12 +10,13 @@ pathIps = "/root/bots/testBots/sendIps/ips.txt"
 seen = {}
 
 
-with open(pathIps, "r", encoding="utf-8") as db:
-    seen = json.load(db)
-
-
-with open(path, "r", encoding="utf-8") as f, \
-     open(pathIps, "w", encoding="utf-8") as out:
+try:
+    with open(pathIps, "r", encoding="utf-8") as db:
+        seen = json.load(db)
+except (FileNotFoundError, json.JSONDecodeError):
+    pass
+    
+with open(path, "r", encoding="utf-8") as f:
     for line in f:
         if "accepted" in line and "from " in line:
             m = re.search(r'\bfrom\s+(\d{1,3}(?:\.\d{1,3}){3}):\d+.*?email:\s*(\S+)', line)
@@ -26,8 +27,9 @@ with open(path, "r", encoding="utf-8") as f, \
             name = m.group(2)
             
             if name not in seen:
-                seen[name] = {}
+                seen[name] = []
             
             if ip not in seen[name]:
-                seen[name] = ip
-    json.dump(seen, out, indent=2)
+                seen[name].append(ip)
+with open(pathIps, "w", encoding="utf-8") as out:
+    json.dump(seen, out, indent=2, sort_keys=True)
