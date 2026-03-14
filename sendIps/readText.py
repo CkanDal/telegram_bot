ef592946890135de79ba8c5005c57f26d784f3dc
Vladimir <Vladimir@mail.ru>
2026-03-14 08:38:35 +0500

    add a nice output IP

--- sendIps/readText.py
+++ sendIps/readText.py
@@ -1,27 +1,33 @@
 import os
 import subprocess
 import re
+import json
+
 
 path = "/var/log/xray/access.log"
 pathIps = "/root/bots/testBots/sendIps/ips.txt"
 
-seen = set()
+seen = {}
+
 
 with open(pathIps, "r", encoding="utf-8") as db:
-    for line in db:
-        seen.add(line.strip())
+    seen = json.load(db)
 
 
 with open(path, "r", encoding="utf-8") as f, \
-     open(pathIps, "a", encoding="utf-8") as out:
+     open(pathIps, "w", encoding="utf-8") as out:
     for line in f:
         if "accepted" in line and "from " in line:
-            m = re.search(r'\bfrom\s+(\d{1,3}(?:\.\d{1,3}){3}):\d+\b', line)
+            m = re.search(r'\bfrom\s+(\d{1,3}(?:\.\d{1,3}){3}):\d+.*?email:\s*(\S+)', line)
             if not m:
                 continue
 
             ip = m.group(1)
-
-            if ip not in seen:
-                seen.add(ip)
-                out.write(ip + "\n")
+            name = m.group(2)
+            
+            if name not in seen:
+                seen[name] = {}
+            
+            if ip not in seen[name]:
+                seen[name] = ip
+    json.dump(seen, out, indent=2)
